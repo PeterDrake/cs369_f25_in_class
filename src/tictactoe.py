80469e921174adca96c9c90d4642f9546a8623e8
Peter Drake <drake@lclark.edu>
2025-09-22 13:41:38 -0700

    Finished Tic-Tac-Toe

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -21,3 +21,48 @@ def winner(state):
         if (state[a] != '.') and (state[a] == state[b] == state[c]):
             return state[a]
     return '.'
+
+def game_over(state):
+    return (legal_moves(state) == []) or (winner(state) != '.')
+
+def value(state, player):
+    if game_over(state):
+        w = winner(state)
+        return 'O.X'.find(w) - 1
+    best_value = -2 if player == 'X' else 2
+    for m in legal_moves(state):
+        v = value(successor(state, player, m), 'O' if player == 'X' else 'X')
+        if (player == 'X' and v > best_value) or (player == 'O' and v < best_value):
+            best_value = v
+    return best_value
+
+# def value_for_x(state):
+#     if game_over(state):
+#         w = winner(state)
+#         return 'O.X'.find(w) - 1
+#     best_value = -2
+#     for m in legal_moves(state):
+#         value = value_for_o(successor(state, 'X', m))
+#         if value > best_value:
+#             best_value = value
+#     return best_value
+#
+# def value_for_o(state):
+#     if game_over(state):
+#         w = winner(state)
+#         return 'O.X'.find(w) - 1
+#     best_value = 2
+#     for m in legal_moves(state):
+#         value = value_for_x(successor(state, 'O', m))
+#         if value < best_value:
+#             best_value = value
+#     return
+
+def best_move(state, player):
+    best_value = -2 if player == 'X' else 2
+    best_play = None
+    for m in legal_moves(state):
+        v = value(successor(state, player, m), 'O' if player == 'X' else 'X')
+        if (player == 'X' and v > best_value) or (player == 'O' and v < best_value):
+            best_value, best_play = v, m
+    return best_play
--- test/test_tictactoe.py
+++ test/test_tictactoe.py
@@ -11,3 +11,21 @@ def test_finds_winner():
 
 def test_finds_diagonal_winner():
     assert winner('OXOXOX.XO') == 'O'
+
+def test_finds_value_at_end_of_game():
+    assert value('XXX...OO.', 'X') == 1
+
+def test_finds_winning_value_at_depth_1():
+    assert value('XX....OO.', 'X') == 1
+
+def test_finds_tie_value_at_depth_1():
+    assert value('XOXXOXO.O', 'X') == 0
+
+def test_finds_deeper_winning_value():
+    assert value('XOXXO.O..', 'X') == 0
+
+def test_finds_best_move_depth_1():
+    assert best_move('XOXXO.O.X', 'O') == 7
+
+def test_finds_best_move_depth_2():
+    assert best_move('XOXXO.O..', 'X') == 7
